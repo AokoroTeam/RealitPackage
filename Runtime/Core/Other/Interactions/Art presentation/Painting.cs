d24ddbdc4058d372ad00c59e0f299d6f9bad597f
LupeniTuikalepa <lupeni.tuikalepa@gmail.com>
2023-08-21 11:39:51 +0200

    Interactions deuxieme etapes

--- Runtime/Core/Other/Interactions/Art presentation/Painting.cs
+++ Runtime/Core/Other/Interactions/Art presentation/Painting.cs
@@ -1,3 +1,4 @@
+using NaughtyAttributes;
 using Realit.Core.Player.Interactions;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,9 +8,47 @@ namespace Realit.Core
 {
     public class Painting : InteractableObject
     {
+        #region Static
+        private static int openHashTrigger;
+        private static int closeHashTrigger;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+        private static void CreateAnimatorHashes()
+        {
+            openHashTrigger = Animator.StringToHash("Open");
+            closeHashTrigger = Animator.StringToHash("Close");
+        }
+        #endregion
+
+        [BoxGroup("Painting")]
+        public Animator uiAnimator;
+
+        private bool isActive = false;
+
+        protected override void Update()
+        {
+            base.Update();
+
+            //If too far and active
+            if(isActive && !PlayerInteractions.interactable.HasChannel(this))
+            {
+                //Deactivate
+                Interact();
+            }
+        }
+
         public override void Interact()
         {
-            Debug.Log("Interacting");
+            isActive = !isActive;
+
+            if(isActive)
+            {
+                uiAnimator.SetTrigger(openHashTrigger);
+            }
+            else
+            {
+                uiAnimator.SetTrigger(closeHashTrigger);
+            }
         }
     }
 }
